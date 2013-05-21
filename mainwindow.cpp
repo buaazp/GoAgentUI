7015b2acd8651db92d4d0e4f9709850816f9cc25
buaazp <ziputao1165@gmail.com>
2013-05-22 04:39:38 +0800

    Important milestone! 1.minisize after start. 2.cout proxy info and check start status more exactly.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     int readSet=readSetting();
     ui->setupUi(this);
     autoStart();
+    autoHideWin();
 }
 
 MainWindow::~MainWindow()
@@ -16,7 +17,6 @@ MainWindow::~MainWindow()
     {
         p->kill();
     }
-    //int saveRet = saveSetting();
     delete ui;
 }
 
@@ -27,10 +27,11 @@ int MainWindow::readSetting()
         configIni = new QSettings(conf, QSettings::IniFormat);
         configIni->setIniCodec("UTF8");
         configIni->beginGroup("Settings");
-//        QString atrun=configIni->value("autoRun").toString();
-//        bool ok;
-//        autoRun = atrun.toInt(&ok, 10);
+        //        QString atrun=configIni->value("autoRun").toString();
+        //        bool ok;
+        //        autoRun = atrun.toInt(&ok, 10);
         autoRun = configIni->value("autoRun").toInt();
+        autoHide = configIni->value("autoHide").toInt();
         filename = configIni->value("filename").toString();
         configIni->endGroup();
         return 1;
@@ -52,44 +53,62 @@ int MainWindow::saveSetting()
         configIni->setIniCodec("UTF8");
         configIni->beginGroup("Settings");
         configIni->setValue("autoRun",autoRun);
+        configIni->setValue("autoHide",autoHide);
         configIni->setValue("filename",filename);
         configIni->endGroup();
-//        QString confPath = configIni->fileName();
-//        QMessageBox::information(this, "Warning", confPath, QMessageBox::Ok);
+        //        QString confPath = configIni->fileName();
+        //        QMessageBox::information(this, "Warning", confPath, QMessageBox::Ok);
         return 1;
     }
 }
 
 int MainWindow::startRun()
 {
+    ui->lbInfo->setText(gainstart);
     p = new QProcess;
+    connect(p, SIGNAL(readyReadStandardOutput()), this, SLOT(redFromStdOut()));
+    connect(p, SIGNAL(readyReadStandardError()), this, SLOT(redFromStdErr()));
+    connect(p, SIGNAL(error(QProcess::ProcessError)), this, SLOT(processError(QProcess::ProcessError)));
+    connect(p, SIGNAL(finished(int,QProcess::ExitStatus)), this, SLOT(processFinished(int,QProcess::ExitStatus)));
     p->start(filename);
     bStart = p->waitForStarted();
-    if(!bStart)
-    {
-        ui->lbInfo->setText(gatimeout);
-    }
-    else
-    {
-        qDebug()<<QString::fromLocal8Bit(p->readAllStandardOutput());
-        ui->lbInfo->setText(gastart);
-    }
+    //bFinish = p->waitForFinished(-1);
     if(bStart)
         return 1;
     else
         return -1;
 }
 
+void MainWindow::autoHideWin()
+{
+    if(autoHide>0)
+    {
+        ui->cbHide->setCheckState(Qt::Checked);
+        if(bStart)
+        {
+            this->showMinimized();
+        }
+    }
+    else
+    {
+        ui->cbHide->setCheckState(Qt::Unchecked);
+    }
+}
+
+int MainWindow::getAutoRun()
+{
+    return autoRun;
+}
+
 void MainWindow::autoStart()
 {
     if(autoRun>0)
-        ui->ckbAuto->setCheckState(Qt::Checked);
-    else
-        ui->ckbAuto->setCheckState(Qt::Unchecked);
-    if(autoRun>0)
     {
+        ui->ckbAuto->setCheckState(Qt::Checked);
         int startret = startRun();
     }
+    else
+        ui->ckbAuto->setCheckState(Qt::Unchecked);
 }
 
 void MainWindow::on_btOpen_clicked()
@@ -120,7 +139,8 @@ void MainWindow::on_btQuit_clicked()
 {
     if(bStart)
     {
-        p->kill();
+        //p->kill();
+        p->close();
         ui->lbInfo->setText(gakill);
         bStart = false;
         //ui->btQuit->setText("Start(A)");
@@ -157,4 +177,76 @@ void MainWindow::on_lbName_linkActivated(const QString &link)
 {
     //QMessageBox::information(this, "Warning", "buaazp!", QMessageBox::Ok);
     QDesktopServices::openUrl(QUrl(QLatin1String(link.toLatin1().data())));
+}
+
+void MainWindow::on_cbHide_stateChanged(int arg1)
+{
+    autoHide = arg1;
+    int saveRet = saveSetting();
+}
+
+
+void MainWindow::redFromStdOut()
+{
+    ui->txtbInfo->append("<font color=green>Std OUT:</font>");
+    ui->txtbInfo->append(p->readAllStandardOutput());
+    ui->txtbInfo->append("<font color=green>Std OUT End:</font>");
+}
+void MainWindow::redFromStdErr()
+{
+    QString strerr = p->readAllStandardError();
+    ui->txtbInfo->append("<font color=red>Std ERROR:</font>");
+    ui->txtbInfo->append(strerr);
+    ui->txtbInfo->append("<font color=red>Std ERROR End.</font>");
+    QString suberr, errline;
+    QStringList sublines;
+    int index = strerr.indexOf(errflag);
+    if(index >= 0)
+    {
+        bStart = false;
+        if(this->isMinimized())
+        {
+            this->showMaximized();
+        }
+        suberr = strerr.right(strerr.length()-index);
+        sublines = suberr.split('\n');
+        errline = sublines.at(0);
+        ui->lbInfo->setText(gafial + "\n" + errline);
+    }
+}
+void MainWindow::processError(QProcess::ProcessError error)
+{
+    if(error==QProcess::FailedToStart)
+    {
+        bStart=false;
+        if(this->isMinimized())
+        {
+            this->showMaximized();
+        }
+        ui->lbInfo->setText(gafial);
+        ui->txtbInfo->append("<font color=red>QProcess start FAILED.</font>");
+    }
+}
+void MainWindow::processFinished(int exitCode,QProcess::ExitStatus exitStatus)
+{
+    if(exitStatus == QProcess::CrashExit)
+    {
+        bStart=false;
+        if(this->isMinimized())
+        {
+            this->showMaximized();
+        }
+        ui->lbInfo->setText(gakill);
+        ui->txtbInfo->append("<font color=red>Proxy.py is TERMINATED.</font>");
+    }
+    else
+    {
+        bStart=false;
+        if(this->isMinimized())
+        {
+            this->showMaximized();
+        }
+        //ui->lbInfo->setText(gafial);
+        ui->txtbInfo->append("<font color=green>Proxy.py END.</font>");
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QProcess>
@@ -9,9 +10,10 @@
 #include <QSettings>
 #include <QDesktopServices>
 #include <QUrl>
+#include <QString>
 
 namespace Ui {
-    class MainWindow;
+class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -24,7 +26,9 @@ public:
     int readSetting();
     int saveSetting();
     void autoStart();
+    void autoHideWin();
     int startRun();
+    int getAutoRun();
     
 private slots:
     void on_btOpen_clicked();
@@ -37,21 +41,35 @@ private slots:
 
     void on_lbName_linkActivated(const QString &link);
 
+    void on_cbHide_stateChanged(int arg1);
+
+    void redFromStdOut();
+
+    void redFromStdErr();
+
+    void processError(QProcess::ProcessError error);
+
+    void processFinished(int exitCode,QProcess::ExitStatus exitStatus);
+
 private:
     Ui::MainWindow *ui;
-    QString gastart="GoAgent Started!";
-    QString gafial="GoAgent Failed!";
-    QString gatimeout="GoAgent Timeout!";
-    QString gakill="GoAgent terminated!";
-    QString garun="GoAgent is running!";
-    QString ganotrun="GoAgent is not running!";
-    QString ganotfind="GoAgent not find!";
+    const QString gainstart="GoAgent is Starting!";
+    const QString gastart="GoAgent Started!";
+    const QString gafial="GoAgent start Failed!";
+    const QString gatimeout="GoAgent start Timeout!";
+    const QString gakill="GoAgent is terminated!";
+    const QString garun="GoAgent is running!";
+    const QString ganotrun="GoAgent is not running!";
+    const QString ganotfind="GoAgent not find!";
+    const QString errflag="error";
     bool bStart = false;
+    bool bFinish = false;
     QSettings *configIni;
-    QString conf="config.ini";
+    const QString conf="gauiconf.ini";
     QProcess *p;
 
     int autoRun = 0;
+    int autoHide = 0;
     QString filename;
 };
 
